a4cc5ef316f133137451e248a5066ae84a51be58
Simon Streblow <SStreblow@gmx.de>
2014-04-17 12:19:50 +0200

    Add TextureManager AudioManager and texturepacker

--- VierGewinnt/src/com/VierGewinnt/VierGewinnt.java
+++ VierGewinnt/src/com/VierGewinnt/VierGewinnt.java
@@ -2,46 +2,27 @@ package com.VierGewinnt;
 
 
 
+import com.VierGewinnt.Modelle.AudioManager;
+import com.VierGewinnt.Modelle.TexturesManager;
 import com.VierGewinnt.screens.Splash;
 import com.badlogic.gdx.Game;
 
-
+	
 
 public class VierGewinnt extends Game {
 
+	
+	
 	public static final String TITLE = "VierGewinnt", VERSION = "0.0.0.0 reallyCarly";
 
 	@Override
 	public void create() {	
+		
+
+		AudioManager.loadAudio();
+		
+		TexturesManager.loadTextures();
+		
 		setScreen(new Splash());
 	}
-
-	@Override
-	public void dispose() {
-		super.dispose();
-	}
-
-	@Override
-	public void render() {		
-		super.render();
-	}
-
-	@Override
-	public void resize(int width, int height) {
-		super.resize(width, height);
-	}
-
-	@Override
-	public void pause() {
-		super.pause();
-	}
-
-	@Override
-	public void resume() {
-		super.resume();
-	}
-
-
-
-	
 }
