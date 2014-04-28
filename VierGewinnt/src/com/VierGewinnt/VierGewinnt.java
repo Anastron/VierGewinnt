e45e07c03910857292fdf6f3181124b9fad3b5c5
Mike Schwörer <mailport@mikescher.de>
2014-04-28 21:27:58 +0200

    Added Network CLient to VierGewinnt.java

--- VierGewinnt/src/com/VierGewinnt/VierGewinnt.java
+++ VierGewinnt/src/com/VierGewinnt/VierGewinnt.java
@@ -2,20 +2,41 @@ package com.VierGewinnt;
 
 import com.VierGewinnt.models.TexturesManager;
 import com.VierGewinnt.screens.LoginScreen;
-import com.VierGewinnt.screens.MainMenuScreen;
-import com.VierGewinnt.screens.Splash;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.esotericsoftware.kryonet.Client;
+import com.viergewinnt.server.tcp_messages.TCPMessage;
 
 public class VierGewinnt extends Game {
 	public static final String TITLE = "VierGewinnt", VERSION = "0.0.0.0 reallyCarly";
 
+	private static VierGewinnt instance = null;
+	
+	private Client networkClient = null;
+	
 	@Override
 	public void create() {	
+		if (instance != null)
+			System.out.println("Can't create mor than one static VG instance");
+		
+		instance = this;
+		
 		//AudioManager.loadAudio();  wait for audio
+		
+		networkClient = new Client();
+		networkClient.start();
+	    TCPMessage.registerKryo(networkClient.getKryo());
 		
 		TexturesManager.loadTextures();
 		
 		((Game) Gdx.app.getApplicationListener()).setScreen(new LoginScreen());
+	}
+	
+	public VierGewinnt getInstance() {
+		return instance;
+	}
+
+	public Client getNetworkClient() {
+		return networkClient;
 	}
 }
